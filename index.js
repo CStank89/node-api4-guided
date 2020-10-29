7fca2171065bddad5abe788c0d05b7855be23916
CStank89 <C.Stank89@gmail.com>
2020-10-29 12:59:52 -0400

    ready

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
+// when running local
+//when running local it will be on 5000 but process.env.PORT
+//is what heroku will run
+const secret = process.env.SECRET_THING;
+
+console.log(port, secret);
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
